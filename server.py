f2029fabbb21accceebb6045ffbdab71314edafd
t-kubrak <chikashikaari@gmail.com>
2017-06-18 12:23:48 -0400

    Show extra info for  the events

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from pprint import pprint
 raw_dataset_df = pd.read_csv('feedbacks.csv')
 
 # Load events data
-events_df = pd.read_csv('events.csv', index_col='event_id')
+events_df = pd.read_csv('events_1.csv', index_col='event_id')
 
 # Convert the running list of user ratings into a matrix
 ratings_df = pd.pivot_table(raw_dataset_df, index='user_id',
@@ -31,7 +31,7 @@ def index():
     return send_from_directory('static', "index.html")
 
 @app.route("/users/<id>")
-def hello(id):
+def recommend_events(id):
     id = int(id)
     reviewed_events_df = raw_dataset_df[raw_dataset_df['user_id'] == id]
     reviewed_events_df = reviewed_events_df.join(events_df, on='event_id')
@@ -42,7 +42,7 @@ def hello(id):
     recommended_df = events_df[events_df.index.isin(already_reviewed) == False]
     recommended_df = recommended_df.sort_values(by=['rating'], ascending=False)
 
-    response = recommended_df[["Activity", "Time", "Location", 'rating']].to_json(orient='index')
+    response = recommended_df[["Activity", "Location", "URL", "Day", "lat", "lng", 'rating']].to_json(orient='index')
     pprint(response)
     return jsonify(response)
 
