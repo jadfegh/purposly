7bc2170a2e2a1571de1f53c37ef34496f7d66a11
t-kubrak <chikashikaari@gmail.com>
2017-06-18 10:04:34 -0400

    Organize code

--- server.py
+++ server.py
@@ -5,11 +5,11 @@ from flask import Flask
 from flask import jsonify
 from pprint import pprint
 
-# Load user ratings
+# Load user feedbacks about events
 raw_dataset_df = pd.read_csv('feedbacks.csv')
 
-# Load movie titles
-movies_df = pd.read_csv('events.csv', index_col='event_id')
+# Load events data
+events_df = pd.read_csv('events.csv', index_col='event_id')
 
 # Convert the running list of user ratings into a matrix
 ratings_df = pd.pivot_table(raw_dataset_df, index='user_id',
@@ -33,13 +33,13 @@ def index():
 @app.route("/users/<id>")
 def hello(id):
     id = int(id)
-    reviewed_movies_df = raw_dataset_df[raw_dataset_df['user_id'] == id]
-    reviewed_movies_df = reviewed_movies_df.join(movies_df, on='event_id')
+    reviewed_events_df = raw_dataset_df[raw_dataset_df['user_id'] == id]
+    reviewed_events_df = reviewed_events_df.join(events_df, on='event_id')
     user_ratings = predicted_ratings[id - 1]
-    movies_df['rating'] = user_ratings
+    events_df['rating'] = user_ratings
 
-    already_reviewed = reviewed_movies_df['event_id']
-    recommended_df = movies_df[movies_df.index.isin(already_reviewed) == False]
+    already_reviewed = reviewed_events_df['event_id']
+    recommended_df = events_df[events_df.index.isin(already_reviewed) == False]
     recommended_df = recommended_df.sort_values(by=['rating'], ascending=False)
 
     response = recommended_df[["Activity", "Time", "Location", 'rating']].to_json(orient='index')
